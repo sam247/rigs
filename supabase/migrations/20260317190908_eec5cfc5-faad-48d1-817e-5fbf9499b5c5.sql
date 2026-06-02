cd8617fdd92a0795e781dbc221a23fd9de163a63
sam247 <sampettiford@googlemail.com>
2026-06-02 13:39:41 +0100

    chore: remove supabase

--- supabase/migrations/20260317190908_eec5cfc5-faad-48d1-817e-5fbf9499b5c5.sql
+++
@@ -1,5 +0,0 @@
--- Make Sam an admin
-UPDATE user_roles SET role = 'admin' WHERE user_id = '229c24ac-9833-4fab-8952-74b61568dacc';
-
--- Re-link some projects to Sam's account so they're visible even in customer view testing
--- But mainly, as admin, all projects will be visible via RLS anyway
